f3a4014dafa236757bfd657b87a3af96927418df
mouradbalayev <muradbalazade@gmail.com>
2024-09-25 14:26:17 +0400

    userModal

--- src/components/Modals/NotifyModal.jsx
+++ src/components/Modals/NotifyModal.jsx
@@ -2,61 +2,89 @@ import { Modal } from "react-responsive-modal";
 import "react-responsive-modal/styles.css";
 
 import { FaBell } from "react-icons/fa";
+import { useState } from "react";
+import UserModal from "./UserModal";
 
 const NotificationModal = ({ isOpen, onClose }) => {
+
+    const [selectedUser, setSelectedUser] = useState(null);
 
     const users = [
         {
             id: 1,
-            name: "John Doe",
+            name: "Murad Balayev",
+            age: 22,
+            gender: "Male",
         },
         {
             id: 2,
-            name: "Jane Doe",
+            name: "Ali Aghayev",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 3,
             name: "Bob Smith",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 4,
             name: "Alice Johnson",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 5,
             name: "Michael Brown",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 6,
             name: "Sarah Davis",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 7,
             name: "David Wilson",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 8,
             name: "Emily Taylor",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 9,
             name: "Daniel Anderson",
+            age: 25,
+            gender: "Male",
         },
         {
             id: 10,
             name: "Olivia Martinez",
+            age: 25,
+            gender: "Male",
         }
 
     ]
+
+    const handleSelectUser = (user) => {
+        setSelectedUser(user);
+    }
 
     return (
         <Modal open={isOpen} onClose={onClose} center>
             <div className="md:p-12 sm:p-8 p-4 bg-gradient-to-br from-[#f3f4f50f] to-[#54be9539]">
                 <div className="text-center my-3 md:mb-0">
                     <h1 className="poppins md:text-3xl text-2xl">Recent Notifications</h1>
-                    <div className="mt-4 overflow-auto max-h-96">
+                    <div  className="mt-4 overflow-auto max-h-96">
                         {users.map((user) => (
-                            <div key={user.id} className="poppins flex items-center flex-start gap-2 my-2 bg-[#EFEFEF] rounded-xl pe-5">
+                            <div onClick={() => handleSelectUser(user)} key={user.id} className="poppins cursor-pointer flex items-center flex-start gap-2 my-2 bg-[#EFEFEF] rounded-xl pe-5">
                                 <FaBell size={40} color="white" className="p-3 bg-[#00704A] rounded-xl"/>
                                 <p className="text-sm"> <span className="text-[#00704A] font-semibold"> {user.name}</span> has entered your CoffeeShop</p>
                             </div>
@@ -64,7 +92,13 @@ const NotificationModal = ({ isOpen, onClose }) => {
                     </div>
                 </div>
             </div>
-        </Modal>
+            {selectedUser && (
+                <UserModal 
+                    isOpen={!!selectedUser} 
+                    onClose={() => setSelectedUser(null)} // Close the modal
+                    user={selectedUser} // Pass selected user to the UserModal
+                />
+            )}        </Modal>
     );
 };
 
--- src/components/Modals/UserModal.jsx
+++ src/components/Modals/UserModal.jsx
@@ -1,10 +1,28 @@
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 
-const UserModal = () => {
+import { Avatar } from "@mui/material";
+
+const UserModal = ({ isOpen, onClose, user }) => {
+
   return (
-    <div>
-      SAlam
-    </div>
-  )
-}
+    <Modal open={isOpen} onClose={onClose} center>
+      <div className="md:p-12 sm:p-8 p-4 bg-gradient-to-br from-[#f3f4f50f] to-[#54be9539]">
+        <div className="flex flex-col gap-2 text-center my-3 md:mb-0">
+          <h1 className="poppins md:text-3xl text-2xl">User information</h1>
+          <div className="mt-4 w-full overflow-auto flex flex-col items-center justify-center">
+            <Avatar alt="Remy Sharp" src={user.image} sx={{ width: 100, height: 100 }} />
+            <p className="text-xl mt-3 font-bold"> {user.name} </p>
+            <div className="poppins shadow-md w-full mt-10 flex flex-col items-start font-semibold justify-center gap-2 my-2 bg-[#d0cfcf] rounded-lg px-4 py-2">
+            <p>Age: {user.age}</p>
+            <p>Gender: {user.gender}</p>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
-export default UserModal
+export default UserModal;
